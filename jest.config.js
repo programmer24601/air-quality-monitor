84bdb72a54161445bad2de9ecbe8d8005581d33d
programmer24601 <matt@mused.biz>
2022-02-06 21:39:59 +0000

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: 'ts-jest',
+    collectCoverageFrom: [
+        '**/*.ts',
+        '!bin/*.ts',
+        '!**/*.test.ts',
+        '!dist/*.ts',
+        '!**/__fixtures__/**',
+    ],
+    testPathIgnorePatterns: ['<rootDir>/dist/'],
+    testEnvironment: 'node',
+    coverageThreshold: {
+        global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100,
+        },
+    },
+    coverageReporters: ['json-summary', 'json', 'lcov', 'text', 'clover'],
+    verbose: true,
+};
