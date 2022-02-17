5144aa27e20cafd67e296d1b5cc4c5341bafec8e
programmer24601 <matt@mused.biz>
2022-02-17 14:49:00 +0000

    use custom char to set degree symbol

--- src/writeToDisplay.test.ts
+++ src/writeToDisplay.test.ts
@@ -2,12 +2,16 @@ import { writeToDisplay } from "./writeToDisplay";
 import { MeasurementData } from "./types/Measurement";
 
 const mockPrintLine = jest.fn();
+const mockSetCursor = jest.fn();
+const mockCreateChar = jest.fn();
 jest.mock("raspberrypi-liquid-crystal", () => {
   return jest.fn().mockImplementation(() => {
     return {
       begin: jest.fn(),
       clear: jest.fn(),
-      printLine: mockPrintLine
+      printLine: mockPrintLine,
+      setCursor: mockSetCursor,
+      createChar: mockCreateChar
     };
   });
 });
@@ -31,5 +35,11 @@ describe("measure", () => {
     expect(mockPrintLine).toHaveBeenNthCalledWith(2, 1, `RH: 45.00 %`);
     expect(mockPrintLine).toHaveBeenNthCalledWith(3, 2, `P: 1020.00 hPa`);
     expect(mockPrintLine).toHaveBeenNthCalledWith(4, 3, `CO2: 850.00 ppm`);
+
+    expect(mockSetCursor).toBeCalledTimes(1);
+    expect(mockSetCursor).toHaveBeenCalledWith(0, 8);
+
+    expect(mockCreateChar).toBeCalledTimes(1);
+    expect(mockCreateChar).toHaveBeenCalledWith(0, [0x1c, 0x14, 0x1c, 0x0, 0x0, 0x0, 0x0]);
   });
 });
--- src/writeToDisplay.ts
+++ src/writeToDisplay.ts
@@ -18,6 +18,10 @@ export const writeToDisplay = async (measurementData: MeasurementData): Promise<
   await lcd.printLine(1, `RH: ${data.relativeHumidity} %`);
   await lcd.printLine(2, `P: ${data.pressure} hPa`);
   await lcd.printLine(3, `CO2: ${data.co2Concentration} ppm`);
+
+  await lcd.setCursor(0, 8);
+  const degreeSymbol = [0x1c, 0x14, 0x1c, 0x0, 0x0, 0x0, 0x0];
+  await lcd.createChar(0, degreeSymbol);
 };
 
 const formatMeasurementData = (measurementData: MeasurementData) => {
